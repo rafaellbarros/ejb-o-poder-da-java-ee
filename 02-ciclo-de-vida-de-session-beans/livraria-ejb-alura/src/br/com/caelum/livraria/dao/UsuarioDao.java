a13a610889589e8707870c495b08d069e5eb4595
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-06 15:09:55 -0300

    :construction: Ciclo de vida de Session Beans

---
+++ 02-ciclo-de-vida-de-session-beans/livraria-ejb-alura/src/br/com/caelum/livraria/dao/UsuarioDao.java
@@ -0,0 +1,18 @@
+package br.com.caelum.livraria.dao;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+
+import br.com.caelum.livraria.modelo.Usuario;
+
+@Stateless
+public class UsuarioDao {
+
+	@Inject
+	private Banco banco;
+
+	public Usuario buscaPeloLogin(String login) {
+		return this.banco.buscaPeloNome(login);
+	}
+	
+}
