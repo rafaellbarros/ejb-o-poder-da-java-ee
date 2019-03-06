486d3e03177a39005df5e6a7204965ffa62ccc49
Rafael Barros <rafaelbarros.df@gmail.com>
2019-03-06 19:06:17 -0300

    :construction: Integração do JPA com Pool e DataSource

---
+++ 03-integracao-do-jpa-com-pool-e-datasource/livraria-ejb-alura/src/br/com/caelum/livraria/dao/AutorDao.java
@@ -0,0 +1,48 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.livraria.modelo.Autor;
+
+@Stateless
+public class AutorDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	@PostConstruct
+	void aposCriacao() {
+		System.out.println("AutorDao foi criado");
+	}
+
+	public void salva(Autor autor) {
+		
+		System.out.println("salvando Autor " + autor.getNome());
+		/*
+		try {
+			Thread.sleep(20000); //20s
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}*/
+		
+		manager.persist(autor);
+		
+		System.out.println("salvou Autor " + autor.getNome());
+	}
+	
+	public List<Autor> todosAutores() {
+		return manager.createQuery("select a from Autor a", Autor.class).getResultList();
+	}
+
+	public Autor buscaPelaId(Integer autorId) {
+		Autor autor = this.manager.find(Autor.class, autorId);
+		return autor;
+	}
+	
+}
---
+++ 03-integracao-do-jpa-com-pool-e-datasource/livraria-ejb-alura/src/br/com/caelum/livraria/dao/LivroDao.java
@@ -0,0 +1,26 @@
+package br.com.caelum.livraria.dao;
+
+import java.util.List;
+
+import javax.ejb.Stateless;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import br.com.caelum.livraria.modelo.Livro;
+
+@Stateless
+public class LivroDao {
+
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public void salva(Livro livro) {
+		manager.persist(livro);
+	}
+	
+	public List<Livro> todosLivros() {
+		return manager.createQuery("select l from Livro l", Livro.class).getResultList();
+	}
+	
+}
